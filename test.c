7b43f0f3f4ed83450f72c9a44fc3cc7ba9840a63
zhangweibo <jessechang2358@gmail.com>
2021-07-12 12:34:54 +0000

    ✨ feat(变量和指针2):

--- test.c
+++ test.c
@@ -3,10 +3,10 @@
 int main(int argc, char const *argv[])
 {
     /* code */
-    int iNum = 0x64;
-    int ptr = &iNum;
-    int **pPtr = &iNum;
+    int length = 20;
+    int width = 40;
+    int area = length + width;
 
-    printf("%x, %x\n", **pPtr, iNum);
+    printf("%d\n", area);
     return 0;
 }
