12df679595247ff69895d13c0c7be92b010dd082
zhangweibo <jessechang2358@gmail.com>
2021-07-12 11:42:41 +0000

    ✨ feat(变量和指针):

--- test.c
+++ test.c
@@ -1,22 +1,12 @@
 #include <stdio.h>
-#include <string.h>
-struct Books
+
+int main(int argc, char const *argv[])
 {
-    char title[50];
-    char author[50];
-    char subject[100];
-    int book_id;
-} book = {"c语言", "clang", "编程语言", 123456};
+    /* code */
+    int iNum = 0x64;
+    int ptr = &iNum;
+    int **pPtr = &iNum;
 
-int main()
-{
-    struct Books book2;
-
-    strcpy(book2.title, "Python");
-    strcpy(book2.author, "van");
-    strcpy(book2.subject, "python tutorial");
-    book2.book_id = 1234567;
-
-    printf("title: %s\nauthor: %s\nsubject: %s\nbookid: %d\n\n", book.title, book.author, book.subject, book.book_id);
-    printf("title: %s\nauthor: %s\nsubject: %s\nbookid: %d\n", book2.title, book2.author, book2.subject, book2.book_id);
+    printf("%x, %x\n", **pPtr, iNum);
+    return 0;
 }
