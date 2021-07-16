77c7e897b3ea483c31c362451dc06bd68c13311f
zhangweibo <jessechang2358@gmail.com>
2021-07-16 12:15:59 +0000

    ✨ feat(变量和指针):

--- test.c
+++ test.c
@@ -2,11 +2,15 @@
 
 int main(int argc, char const *argv[])
 {
-    /* code */
-    int length = 20;
-    int width = 40;
-    int area = length + width;
+    typedef int INT64;
 
-    printf("%d\n", area);
+    INT64 a;
+    a = 123;
+    printf("%d\n", a);
+
+    float b;
+    (INT64) b; //这里的类型转换不管用
+    b = 456;
+    printf("%d\n", b);
     return 0;
 }
